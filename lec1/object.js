64301fc012f6bbe18d4d8da24a0f15027ba30e03
Nikhal kumar <nikhalkumar0205@gamil.com>
2026-07-14 12:25:13 +0530

    seal_freeze

--- lec1/object.js
+++ lec1/object.js
@@ -14,23 +14,52 @@
 // person2.start();
 
 
-class Myclass{
-    constructor(name, age , city){
-        this.name = name;             //.name is just a variable
-        this.age = age;
-        this.city = city;
-    }
+// class Myclass{
+//     constructor(name, age , city){
+//         this.name = name;             //  .name is just a variable
+//         this.age = age;
+//         this.city = city;
+//     }
+//     start(){
+//         console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old and I live in " + this.city);
+//     }
+// }
 
-    
-    start(){
-        console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old and I live in " + this.city);
-    }
-}
-
-const someperson = new Myclass("Reet", 19, "Ambala");
-const person2 = new Myclass("Nikhal", 19, "barnala");
-someperson.start();
-person2.start();
+// const someperson = new Myclass("Reet", 19, "Ambala");
+// const person2 = new Myclass("Nikhal", 19, "barnala");
+// someperson.start();
+// person2.start();
 
 // console.log(someperson);
 // console.log(person2);
+
+
+var obj2 = {
+    name: "Nikhal",
+    age: 19,
+    friend: {
+        name: "Alice",
+        age: 20
+    }
+}
+
+// seal is used to prevent adding or removing properties from an object, but allows modification of existing properties.
+Object.seal(obj2);
+obj2.myage = 20;
+obj2.name = "Reet";
+console.log(obj2.name); 
+console.log(obj2.myage);
+
+Object.freeze(obj2);
+obj2.myage = 20;
+obj2.name = "Reet";
+console.log(obj2.name); 
+console.log(obj2.myage);
+
+
+
+// const {friend :{name,age}}= obj2;
+
+// console.log(name);
+// console.log(age);  
+
